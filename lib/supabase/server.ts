090d349df5c8e312588c8559b44ac3a11cfb9d64
kruntum <kruntum@hotmail.com>
2025-12-03 12:07:33 +0700

    feat: Implement Supabase integration with browser, server, and middleware clients, and remove `supabase/` from .gitignore.

---
+++ lib/supabase/server.ts
@@ -0,0 +1,40 @@
+
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { createClient as createSupabaseClient } from '@supabase/supabase-js';
+import { cookies } from "next/headers";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export const createClient = async () => {
+    const cookieStore = await cookies()
+
+    return createServerClient(
+        supabaseUrl!,
+        supabaseKey!,
+        {
+            cookies: {
+                getAll() {
+                    return cookieStore.getAll()
+                },
+                setAll(cookiesToSet) {
+                    try {
+                        cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options))
+                    } catch {
+                        // The `setAll` method was called from a Server Component.
+                        // This can be ignored if you have middleware refreshing
+                        // user sessions.
+                    }
+                },
+            },
+        },
+    );
+};
+
+// Admin Supabase client (Bypasses RLS - ONLY use in secure server-side API routes)
+export const getServiceSupabase = () => {
+    return createSupabaseClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.SUPABASE_SERVICE_ROLE_KEY!
+    );
+};
